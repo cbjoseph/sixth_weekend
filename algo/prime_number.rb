900bf80fc86f1b50f376f898ebe4b63d97f86a8e
Caroline Joseph <carbroj@gmail.com>
2016-08-05 09:28:37 -0500

    used require prime

--- algo/prime_number.rb
+++ algo/prime_number.rb
@@ -1,22 +1,21 @@
 # Use RSpec to test the following exercise! You may wish to wrap the methods below in a class to help facilitate this.
-
+require 'prime'
 # First, implement the is_prime? method below. It should accept a number as an argument and return
 # true if the number is prime and false if it is not. A prime number is a number that is only
 # divisible by itself and the number 1.
 
+
 def is_prime?(number)
-  if number / 
+  if Prime.instance.prime?(number)
     puts true
-  elsif number / 1 == number
-    puts true
-  else 
+  else
     puts false
   end
 end
 
-# Second, implement the highest_prime_number_under method below. It should accept a number as
-# an argument and return the highest prime number under that number. For example, the 
-# highest prime number under 10 is 7.
+# # Second, implement the highest_prime_number_under method below. It should accept a number as
+# # an argument and return the highest prime number under that number. For example, the 
+# # highest prime number under 10 is 7.
 
 def highest_prime_number_under(number)
 
