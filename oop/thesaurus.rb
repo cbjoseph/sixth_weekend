b29eab59a19b876f8330e4054109265abbc6a040
Caroline Joseph <carbroj@gmail.com>
2016-08-08 11:43:49 -0500

    able to add word to thesaurus

--- oop/thesaurus.rb
+++ oop/thesaurus.rb
@@ -1,3 +1,4 @@
+require 'rspec'
 # Create a thesaurus application. To create it, you will create two classes, Thesaurus and Entry.
 # A Thesaurus will contain many Entries. An Entry contains three primary attributes:
 # word, synonymns, and antonyms. Your application should contain the following features:
@@ -12,48 +13,56 @@
 # and which belongs in the Entry class.
 #
 # And... test your functionality using RSpec!
+#  def initialize(word)
+#     @words = []
+#     @words << word
+#     @word = word[:word]
+#     @synonym = word[:synonym]
+#     @antonym = word[:antonym]
+#   end
+#   def find_synonym
+#     puts @synonym
+#   end
 
+#   def find_antonym
+#     puts @antonym
+#   end
+
+#   def all_words
+#     puts @words
+#   end
+# end
+# class Entry < Thesaurus
+#   def add_new_word(new_word)
+#     @words << new_word
+#   end
+#   def add_synonym(synonym)
+#   end
+#   def add_antonym(antonym)
+#   end
+# end
+# thesaurus = Entry.new(word: "happy", synonym: "joyful", antonym: "angry")
+# thesaurus.find_synonym
+# thesaurus.find_antonym
+# thesaurus.add_new_word({word: "sad"})
+# thesaurus.all_words
 class Thesaurus
-  def initialize(word)
+  def initialize(input)
     @words = []
-    @words << word
-    @word = word[:word]
-    @synonym = word[:synonym]
-    @antonym = word[:antonym]
+    @words << input[:word]
   end
-
-  def find_synonym
-    puts @synonym
-  end
-
-  def find_antonym
-    puts @antonym
-  end
-
-  def all_words
-    puts @words
-  end
-end
-
-class Entry < Thesaurus
   
-  def add_new_word(new_word)
-    @words << new_word
+  def add_new_word(word)
+    @words << word
+    return @words
   end
-
-  def add_synonym(synonym)
-  end
-
-  def add_antonym(antonym)
-  end
-
 end
 
-thesaurus = Entry.new(word: "happy", synonym: "joyful", antonym: "angry")
-
-thesaurus.find_synonym
-thesaurus.find_antonym
-
-thesaurus.add_new_word({word: "sad"})
-
-thesaurus.all_words
+RSpec.describe Thesaurus do
+  describe '#add_new_word' do
+    it 'should return a word if given a word' do
+      thesaurus = Thesaurus.new(word: "happy")
+      expect(thesaurus.add_new_word("basketball")).to eq(["happy", "basketball"])
+    end
+  end
+end
